cde41c57965ff3cf1ee7fd2eb57a9630a41560ee
Chris Berry <chris.berry07@gmail.com>
2019-03-05 11:14:44 -0600

    flipped button color sorted select by author added gitignore

--- ui/pages/manage.vue.js
+++ ui/pages/manage.vue.js
@@ -1,5 +1,5 @@
-var manage = Vue.component("Manage",{
-    template: `<div>
+var manage = Vue.component("Manage", {
+  template: `<div>
    <h4>{{ message }}</h4>
    <hr />
     <form>
@@ -12,14 +12,14 @@ var manage = Vue.component("Manage",{
             <label for="formGroupQuoteTextInput">Quote Text</label>
             <input type="text" class="form-control" id="formGroupQuoteTextInput" placeholder="Quote text" v-model="quote.text">
         </div>
-        <button type="submit" class="btn btn-success" @click="save">Save Quote</button>
-        <button type="button" class="btn btn-outline-danger" @click="cancel">Cancel</button> 
+<button type="submit" class="btn btn-outline-success" @click="save">Save Quote</button>
+        <button type="button" class="btn btn-danger" @click="cancel">Cancel</button> 
     </form> 
 </div>
 `,
- 
-data() {
-  return {
+
+  data() {
+    return {
       message: 'Add or update a quote',
       quote: {
         id: undefined,
@@ -30,49 +30,49 @@ data() {
       error: ''
     };
   },
-    methods: {
-      load(){
-        let id = this.$route.params.id;
-        if(id){
-          axios
+  methods: {
+    load() {
+      let id = this.$route.params.id;
+      if (id) {
+        axios
           .get(baseUrl + '/quote/' + id)
           .then(response => (this.quote = response.data));
-        }
-      },
+      }
+    },
 
-      save(){   
-        if(this.quote.author && this.quote.text){
-          if(this.quote.id){
-            axios.put(baseUrl + '/quote/',  this.quote)
-                 .then(function(response){
-                   this.success = response.data;
-                   if(this.success){
-                    router.push('/');
-                   }
-                 }).catch(function (error) {
-                  this.error = error;
-                });
-          } else {
-            axios.post(baseUrl + '/quote/',  this.quote)
+    save() {
+      if (this.quote.author && this.quote.text) {
+        if (this.quote.id) {
+          axios.put(baseUrl + '/quote/', this.quote)
             .then(function (response) {
               this.success = response.data;
-              if(this.success){
+              if (this.success) {
+                router.push('/');
+              }
+            }).catch(function (error) {
+              this.error = error;
+            });
+        } else {
+          axios.post(baseUrl + '/quote/', this.quote)
+            .then(function (response) {
+              this.success = response.data;
+              if (this.success) {
                 router.push('/');
               }
             })
             .catch(function (error) {
               this.error = error;
             });
-          }
-        }        
-      },
-
-      cancel() {
-        router.push('/');
+        }
       }
     },
-    
-    mounted() {
-     this.load();
+
+    cancel() {
+      router.push('/');
     }
+  },
+
+  mounted() {
+    this.load();
+  }
 });

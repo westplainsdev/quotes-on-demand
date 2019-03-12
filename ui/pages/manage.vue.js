666202f68f3f257430298673022df62007a62b97
Chris Berry <chris.berry07@gmail.com>
2019-03-12 11:37:44 -0500

    added error messaging for missing form fields

--- ui/pages/manage.vue.js
+++ ui/pages/manage.vue.js
@@ -1,78 +1,106 @@
-var manage = Vue.component("Manage", {
-  template: `<div>
+var manage = Vue.component("Manage",{
+    template: `<div>
    <h4>{{ message }}</h4>
    <hr />
+   <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="error in errors" class="text-danger">{{ error }}</li>
+    </ul>
+  </p>
     <form>
         <input type="hidden" v-model="quote.id" />
         <div class="form-group">
             <label for="formGroupAuthorInput">Author:</label>
-            <input type="text" class="form-control" id="formGroupAuthorInput" placeholder="Author Name" v-model="quote.author">
+            <input type="text" class="form-control" id="formGroupAuthorInput" placeholder="Author Name" v-model.trim="quote.author">
         </div>
         <div class="form-group">
             <label for="formGroupQuoteTextInput">Quote Text</label>
-            <input type="text" class="form-control" id="formGroupQuoteTextInput" placeholder="Quote text" v-model="quote.text">
+            <input type="text" class="form-control" id="formGroupQuoteTextInput" placeholder="Quote text" v-model.trim="quote.text">
         </div>
-<button type="submit" class="btn btn-outline-success" @click="save">Save Quote</button>
+        <button type="submit" class="btn btn-outline-success" @click="save">Save Quote</button>
         <button type="button" class="btn btn-danger" @click="cancel">Cancel</button> 
     </form> 
 </div>
 `,
-
-  data() {
-    return {
-      message: 'Add or update a quote',
+ 
+data() {
+  return {
+      message: '',
       quote: {
         id: undefined,
         author: '',
         text: ''
       },
       success: false,
-      error: ''
+      error: '',
+      errors: []
     };
   },
-  methods: {
-    load() {
-      let id = this.$route.params.id;
-      if (id) {
-        axios
+    methods: {
+      load(){
+        let id = this.$route.params.id;
+        if(id){
+          this.message = 'Edit a quote';
+          axios
           .get(baseUrl + '/quote/' + id)
           .then(response => (this.quote = response.data));
-      }
-    },
-
-    save() {
-      if (this.quote.author && this.quote.text) {
-        if (this.quote.id) {
-          axios.put(baseUrl + '/quote/', this.quote)
-            .then(function (response) {
-              this.success = response.data;
-              if (this.success) {
-                router.push('/');
-              }
-            }).catch(function (error) {
-              this.error = error;
-            });
         } else {
-          axios.post(baseUrl + '/quote/', this.quote)
+          this.message = 'Add a quote';
+        }
+        
+      },
+
+      checkForm(){
+        if (this.quote.author && this.quote.text) {
+          return true;
+        }  
+        this.errors = [];
+  
+        if (!this.quote.author) {
+          this.errors.push('Author name is required.');
+        }
+        if (!this.quote.text) {
+          this.errors.push('Quote text is required.');
+        }
+        if(this.errors.length > 0){
+          return false;
+        }
+      },
+
+      save(){   
+        if(this.checkForm()){
+          if(this.quote.id){
+            axios.put(baseUrl + '/quote/',  this.quote)
+                 .then(function(response){
+                   this.success = response.data;
+                   if(this.success){
+                    router.push('/');
+                   }
+                 }).catch(function (error) {
+                  this.error = error;
+                });
+          } else {
+            axios.post(baseUrl + '/quote/',  this.quote)
             .then(function (response) {
               this.success = response.data;
-              if (this.success) {
+              if(this.success){
                 router.push('/');
               }
             })
             .catch(function (error) {
               this.error = error;
             });
-        }
+          }
+        }        
+      },
+
+      cancel() {
+        router.push('/');
       }
     },
-
-    cancel() {
-      router.push('/');
+    
+    mounted() {
+     this.load();
     }
-  },
-
-  mounted() {
-    this.load();
-  }
 });
